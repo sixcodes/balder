7f64663949d0d6692f7a476fabaa39ed25e774ad
Gabriel Seixas <salesseixasgabriel@gmail.com>
2017-12-08 01:09:00 -0200

    Removed os and sys modules

--- src/balder.py
+++ src/balder.py
@@ -1,5 +1,3 @@
-import sys
-import os
 from threading import Thread
 
 from crawlers.dedalus import crawl_dedalus
@@ -7,7 +5,6 @@ from datasource import DBConnection
 
 
 if __name__ == '__main__':
-    sys.path.insert(0, os.path.abspath('balder.py'))
     DBConnection()
 
     t_dedalus = Thread(target=crawl_dedalus, args=('9788547302177',))
