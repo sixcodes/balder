0895b2d904940e32edc40031b88bebe92bc75de7
Gabriel Seixas <salesseixasgabriel@gmail.com>
2017-12-13 01:49:27 -0200

    Resolves #2

--- src/models.py
+++ src/models.py
@@ -4,10 +4,10 @@ from datasource import DBConnection
 
 class Book(Document):
 
-    # isbn = StringField(default='', unique=True)
-    # name = StringField(default='')
-    # author = StringField(default=None)
-    # place = StringField(default=None)
-    # year = StringField(default=None)
-    # publisher = StringField(default=None)
-    # library = StringField(default=None)
+    isbn = StringField(default='', unique=True)
+    name = StringField(default='')
+    author = StringField(default=None)
+    place = StringField(default=None)
+    year = StringField(default=None)
+    publisher = StringField(default=None)
+    library = StringField(default=None)
